e5d6285ad549198006c697ca3078ebbb4ab2b89f
Jonatan Markusson <jonatan.markusson@gmail.com>
2015-05-11 12:02:10 +0200

    Ändrade knappar

--- src/typietype/TTStartFrame.java
+++ src/typietype/TTStartFrame.java
@@ -35,13 +35,41 @@ public class TTStartFrame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+<<<<<<< Updated upstream
         jButton1.setText("jButton1");
+=======
+        jTextPane1.setFont(new java.awt.Font("Impact", 0, 48)); // NOI18N
+        jTextPane1.setText("TypieType¡");
+        jScrollPane1.setViewportView(jTextPane1);
+
+        jTextPane2.setFont(new java.awt.Font("Impact", 0, 24)); // NOI18N
+        jTextPane2.setText("Svårighetsgrad");
+        jScrollPane2.setViewportView(jTextPane2);
+
+        jButton1.setFont(new java.awt.Font("Impact", 0, 13)); // NOI18N
+        jButton1.setText("Lätt");
+        jButton1.setBorder(null);
+>>>>>>> Stashed changes
         jButton1.setMaximumSize(new java.awt.Dimension(98, 29));
         jButton1.setMinimumSize(new java.awt.Dimension(98, 29));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
-        jButton2.setText("jButton2");
+        jButton2.setFont(new java.awt.Font("Impact", 0, 13)); // NOI18N
+        jButton2.setText("Medel");
+        jButton2.setBorder(null);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
-        jButton3.setText("jButton3");
+        jButton3.setFont(new java.awt.Font("Impact", 0, 13)); // NOI18N
+        jButton3.setText("Svår");
+        jButton3.setBorder(null);
 
         jLabel1.setFont(new java.awt.Font("Impact", 0, 48)); // NOI18N
         jLabel1.setText("TypieType¡");
@@ -88,6 +116,14 @@ public class TTStartFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
